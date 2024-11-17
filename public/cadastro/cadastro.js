b38abaa3eaff9d2a51bb0730f6a69485b9fb77b6
PedroLucasCG <ContatoPedroLucasCG@gmail.com>
2024-11-17 09:36:19 -0300

    mascara de input

---
+++ public/cadastro/cadastro.js
@@ -0,0 +1,5 @@
+import Inputmask from "/node_modules/inputmask/dist/inputmask.es6.js";
+
+Inputmask({"mask": "(99) 9 9999-9999"}).mask(telefone);
+Inputmask({"mask": "999.999.999-99"}).mask(cpf);
+Inputmask({"mask": "99/99/9999"}).mask(data_nasc);
